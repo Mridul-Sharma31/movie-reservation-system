3c15cda047e8a562cda89989a1124195ed0704fe
mridul sharma <Mridulsharma3130@gmail.com>
2026-04-22 10:00:48 +0530

    refactor global error handler: improve code readability and maintainability by adjusting indentation and formatting

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,20 +2,21 @@ import { apiError } from "../utils/apiError.js";
 
 //* next(param)
 export const globalErrorHandler = (err, req, res, next) => {
+  console.error("🔥 ACTUAL ERROR STACK:", err.stack);
 
-    if (err instanceof apiError) {
-        return res.status(err.statuscode).json({
-            success: false,
-            message: err.message,
-            errors: err.errors || []
-        });
-    }
-
-    //if unhandled error then -  
-    console.error("UNHANDLED ERROR:", err.stack); // Log the real error
-    return res.status(500).json({
-        success: false,
-        message: "Internal Server Error",
-        errors: []
+  if (err instanceof apiError) {
+    return res.status(err.statuscode).json({
+      success: false,
+      message: err.message,
+      errors: err.errors || [],
     });
+  }
+
+  //if unhandled error then -
+  console.error("UNHANDLED ERROR:", err.stack); // Log the real error
+  return res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+    errors: [],
+  });
 };
