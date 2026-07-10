66b1fe83c7d8fcdcdcb626bced19f57ec93f426e
mridul sharma <Mridulsharma3130@gmail.com>
2026-07-11 01:22:01 +0530

    fix: correct import name for ApiResponse in auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import { User } from "../models/user.model.js";
 import { apiError } from "../utils/apiError.js";
-import { apiResponse } from "../utils/ApiResponse.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 import jwt from "jsonwebtoken";
 
 //* helper function to generate tokens
